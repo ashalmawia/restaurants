544e11b497c78ffc8843ec5a182c767ec2760a57
ashalmawia <ashalmawia.it@gmail.com>
2019-02-27 15:41:09 +0100

    Add mock restaurants data

--- app/src/main/java/com/ashalmawia/foursquare/features/map/RestaurantsMapPresenter.kt
+++ app/src/main/java/com/ashalmawia/foursquare/features/map/RestaurantsMapPresenter.kt
@@ -20,7 +20,12 @@ class RestaurantsMapPresenterImpl(private val view: RestaurantsMapView) : Restau
     private fun getCurrentLocation() = LatLng(52.3680, 4.9036)
 
     // stub
-    private fun getRestaurants() = listOf<Restaurant>()
+    private fun getRestaurants() = listOf(
+        Restaurant("Starbucks", LatLng(52.36607678472145, 4.897430803910262)),
+        Restaurant("Manneken Pis", LatLng(52.37570751971382, 4.896236799445381)),
+        Restaurant("Bocca", LatLng(52.36443129817435, 4.886863899263005)),
+        Restaurant("Oriental City", LatLng(52.371911733724474, 4.895993555789719))
+    )
 
     override fun stop() {
     }
